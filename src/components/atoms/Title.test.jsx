9b005d2f4e0cce7681054a0b38320b0dc244e2f3
Christelle Quilang <christellequilang@protonmail.com>
2022-11-18 17:35:03 +1300

    Delete static title test

--- src/components/atoms/Title.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react'
-import { expect, test } from 'vitest'
-import Title from './Title'
-
-test('title renders', () => {
-  const title = render(<Title />)
-  expect(title).toMatchSnapshot()
-})
