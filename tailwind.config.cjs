934f96015b5d2c53d18e45c7f8ed833a2e7941cb
Christelle Quilang <christellequilang@protonmail.com>
2022-11-18 17:26:40 +1300

    Add custom fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: [
+          'Clear\\ Sans',
+          'Helvetica\\ Neue',
+          'Arial',
+          'sans-serif',
+          ...defaultTheme.fontFamily.serif,
+        ],
+        stymie: ['Stymie'],
+      },
+    },
   },
   plugins: [],
 }
